7251a7565a395a6d4730e96dc04f05ce5ab698be
paula <paulayaqueline.benitez@gracia.lasalle.cat>
2024-11-17 18:15:37 +0100

    version final

--- src/BenitezPaulaEjercicio1.java
+++ src/BenitezPaulaEjercicio1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BenitezPaula {
+public class BenitezPaulaEjercicio1 {
     public static void main (String [] args){
         Scanner sc= new Scanner(System.in);
 
